a6bad9787a0ebb9dc805c327c6a652cbc3e857f0
TheoVL852 <theovisconti@icloud.com>
2026-03-24 14:43:32 -0300

    Update bandeiras.py

--- bandeiras.py
+++ bandeiras.py
@@ -235,4 +235,173 @@ t.end_fill()
 sleep(2)
 t.clear()
 
+# Bandeira Botswana (50XP)
+
+inicio()
+
+t.color('#0099CC')
+t.begin_fill()
+t.fillcolor('#0099CC')
+for _ in range(2):
+    t.fd(600)
+    t.lt(90)
+    t.fd(300)
+    t.lt(90)
+t.end_fill()
+
+t.lt(90)
+t.fd(120)
+t.rt(90)
+t.color('white')
+t.begin_fill()
+t.fillcolor('white')
+
+for _ in range(2):
+    t.fd(600)
+    t.lt(90)
+    t.fd(60)
+    t.lt(90)
+t.end_fill()
+
+t.lt(90)
+t.fd(10)
+t.rt(90)
+t.color('black')
+t.begin_fill()
+t.fillcolor('black')
+
+for _ in range(2):
+    t.fd(600)
+    t.lt(90)
+    t.fd(40)
+    t.lt(90)
+t.end_fill()
+
+sleep(2)
+t.clear()
+
+# Bandeira Myanmar (50XP)
+
+inicio()
+bandeira()
+
+ret2('#EE2737')
+t.pu()
+t.goto(-300,-50)
+t.pd()
+ret2('#43B02A')
+t.pu()
+t.goto(-300,50)
+t.pd()
+ret2('#FFCD00')
+
+t.pu()
+t.goto(-100,20)
+t.pd()
+t.color('white')
+t.begin_fill()
+t.fillcolor('white')
+for _ in range(5):
+   t.fd(80)
+   t.lt(72)
+   t.fd(80)
+   t.rt(144)
+t.end_fill()
+
+sleep(2)
+t.clear()
+# Bandeira Panama (50XP)
+
+inicio()
+bandeira()
+
+t.color('#072357')
+t.begin_fill()
+t.fillcolor('#072357')
+for _ in range(2):
+    t.fd(300)
+    t.lt(90)
+    t.fd(150)
+    t.lt(90)
+t.end_fill()
+
+t.pu()
+t.goto(0,0)
+t.pd()
+
+t.color('red')
+t.begin_fill()
+t.fillcolor('red')
+for _ in range(2):
+    t.fd(300)
+    t.lt(90)
+    t.fd(150)
+    t.lt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-200,85)
+t.pd()
+
+t.color('#072357')
+t.begin_fill()
+t.fillcolor('#072357')
+for _ in range(5):
+   t.fd(40)
+   t.lt(72)
+   t.fd(40)
+   t.rt(144)
+t.end_fill()
+
+t.pu()
+t.goto(100,-70)
+t.pd()
+
+t.color('red')
+t.begin_fill()
+t.fillcolor('red')
+for _ in range(5):
+   t.fd(40)
+   t.lt(72)
+   t.fd(40)
+   t.rt(144)
+t.end_fill()
+
+sleep(2)
+t.clear()
+
+# Bandeira Finlândia (50XP) 
+
+inicio()
+bandeira()
+t.lt(90)
+t.fd(110)
+t.rt(90)
+
+t.color('#002F6C')
+t.begin_fill()
+t.fillcolor('#002F6C')
+for _ in range(2):
+    t.fd(600)
+    t.lt(90)
+    t.fd(80)
+    t.lt(90)
+t.end_fill()
+
+inicio()
+
+t.fd(120)
+t.color('#002F6C')
+t.begin_fill()
+t.fillcolor('#002F6C')
+for _ in range(2):
+    t.fd(80)
+    t.lt(90)
+    t.fd(300)
+    t.lt(90)
+t.end_fill()
+
+sleep(2)
+t.clear()
+
 mainloop()
